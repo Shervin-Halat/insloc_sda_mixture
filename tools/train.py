774ec5473926353127892e92662749d369f3a1c3
Shervin <sherwinhalat@gmail.com>
2022-08-10 08:56:19 -0400

    first commit

--- tools/train.py
+++ tools/train.py
@@ -19,7 +19,8 @@ from mmdet.utils import collect_env, get_root_logger
 
 def parse_args():
     parser = argparse.ArgumentParser(description='Train a detector')
-    parser.add_argument('config', help='train config file path')
+    # parser.add_argument('--config', default= '/home/lshervnl/test3/InstanceLoc/configs/FPN/insloc_fpn_200ep.py' , help='train config file path') #####
+    parser.add_argument('--config', default= '/home/lshervnl/test3/InstanceLoc/configs/FPN/insloc_fpn_200ep.py' , help='train config file path')
     parser.add_argument('--work-dir', help='the dir to save logs and models')
     parser.add_argument(
         '--resume-from', help='the checkpoint file to resume from')
